f908d8a26597aa711c0e5c1f5f70be64c5fdf917
cwils060 <corinne.m.wilson@gmail.com>
2023-10-16 22:45:25 -0400

    add post model

---
+++ Data/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿namespace gachiBlazorServer.Data.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string PostText { get; set; } 
+
+        public List<string>? CategoryTags { get; set; }
+
+        public List<string> LocationTags{ get; set; }
+
+        public PostType Type { get; set; }
+
+        public int? VoteCount { get; set; }
+
+        public User? User { get; set; }
+    }
+}
