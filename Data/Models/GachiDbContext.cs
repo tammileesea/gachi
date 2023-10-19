0494031f8c0c1800656b0824d746d79e1fe3fe5e
Kateryna Bozhenko <kateryna.bozhenko@gmail.com>
2023-10-18 17:55:19 -0700

    Added db context file

---
+++ Data/Models/GachiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace gachiBlazorServer.Data.Models
+{
+    public class GachiDbContext : DbContext
+    {
+        public GachiDbContext( DbContextOptions<GachiDbContext> options ) : base( options ) { }
+
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<OrganizationAddress> OrganizationAddresses { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
